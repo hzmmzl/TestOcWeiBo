de4d75f59b4ec4e904eed405f55f8aa49309c88b
mzl <you@example.com>
2016-08-03 14:37:12 +0800

    集成pods

---
+++ TestOCWeiBo/TestOCWeiBo/Home(首页)/Controller/IWHomeViewController.m
@@ -0,0 +1,33 @@
+//
+//  IWHomeViewController.m
+//  ItcastWeibo
+//
+//  Created by apple on 14-5-5.
+//  Copyright (c) 2014年 itcast. All rights reserved.
+//
+
+#import "IWHomeViewController.h"
+
+@interface IWHomeViewController ()
+
+@end
+
+@implementation IWHomeViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+}
+
+#pragma mark - Table view data source
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 0;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 0;
+}
+@end
---
+++ TestOCWeiBo/TestOCWeiBo/Message(消息)/Controller/IWMessageViewController.m
@@ -0,0 +1,29 @@
+//
+//  IWMessageViewController.m
+//  ItcastWeibo
+//
+//  Created by apple on 14-5-5.
+//  Copyright (c) 2014年 itcast. All rights reserved.
+//  消息
+
+#import "IWMessageViewController.h"
+
+@interface IWMessageViewController ()
+
+@end
+
+@implementation IWMessageViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 0;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 0;
+}@end
